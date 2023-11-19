2fdc782ba6e98fcb8da564d215ecb9d3e6962dba
Jhonathan <guarino90@gmail.com>
2023-11-19 01:36:04 -0300

    feat: Create a function that transforms the date data into a DD/MM/YYYY format

--- src/components/NewsCard/index.jsx
+++ src/components/NewsCard/index.jsx
@@ -6,10 +6,33 @@ const NewsCard = ({ data }) => {
   const [isFav, setFav] = useState(false);
   const handleClick = () => setFav((prevFav) => !prevFav);
 
+  const transformedDate = () => {
+    const monthAbbreviations = {
+      "01": "jan",
+      "02": "fev",
+      "03": "mar",
+      "04": "abr",
+      "05": "mai",
+      "06": "jun",
+      "07": "jul",
+      "08": "ago",
+      "09": "set",
+      "10": "out",
+      "11": "nov",
+      "12": "dez",
+    };
+
+    const day = data.publishedAt.slice(8, 10);
+    const month = monthAbbreviations[data.publishedAt.slice(5, 7)];
+    const year = data.publishedAt.slice(0, 4);
+
+    return `${day} de ${month}, ${year}`;
+  };
+
   return (
     <article className="card">
       <div className="card__header">
-        <p className="card-header__date">{data.publishedAt.slice(0, 10)}</p>
+        <p className="card-header__date">{transformedDate()}</p>
         <button onClick={handleClick}>
           {isFav ? <FaHeart size={25} /> : <FaRegHeart size={25} />}
         </button>
